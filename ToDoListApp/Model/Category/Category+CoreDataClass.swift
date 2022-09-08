64f000a0fd9888d3b49b4ef63861a833399250f1
Mphathi Maapola <56199797+perayepat@users.noreply.github.com>
2022-09-08 21:16:52 +0200

    Categories are being added

---
+++ ToDoListApp/Model/Category/Category+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Category+CoreDataClass.swift
+//  ToDoListApp
+//
+//  Created by Pat on 2022/09/08.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Category)
+public class Category: NSManagedObject {
+
+}
---
+++ ToDoListApp/Model/Category/Category+CoreDataProperties.swift
@@ -0,0 +1,44 @@
+//
+//  Category+CoreDataProperties.swift
+//  ToDoListApp
+//
+//  Created by Pat on 2022/09/08.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Category {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Category> {
+        return NSFetchRequest<Category>(entityName: "Category")
+    }
+
+    @NSManaged public var catDescription: String?
+    @NSManaged public var catName: String?
+    @NSManaged public var todoListItem: NSSet?
+
+}
+
+// MARK: Generated accessors for todoListItem
+extension Category {
+
+    @objc(addTodoListItemObject:)
+    @NSManaged public func addToTodoListItem(_ value: ToDoListItem)
+
+    @objc(removeTodoListItemObject:)
+    @NSManaged public func removeFromTodoListItem(_ value: ToDoListItem)
+
+    @objc(addTodoListItem:)
+    @NSManaged public func addToTodoListItem(_ values: NSSet)
+
+    @objc(removeTodoListItem:)
+    @NSManaged public func removeFromTodoListItem(_ values: NSSet)
+
+}
+
+extension Category : Identifiable {
+
+}
